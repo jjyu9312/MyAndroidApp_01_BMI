7cd331c79114d6454a7fc922dd7a55847f4a661d
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-02-08 23:28:49 +0900

    Initial commit

---
+++ app/src/main/java/com/kkuber/myapp_01_bmi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kkuber.myapp_01_bmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
