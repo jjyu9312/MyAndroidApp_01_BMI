812ace5856c04408f6b069e1f4f819a8f762ab9b
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-02-11 23:49:18 +0900

    22/02/11 \n BMI 미니 어플 화면 꾸미기

--- app/src/main/java/com/kkuber/myapp_01_bmi/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_01_bmi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kkuber.myapp_01_bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val heightEditText : EditText = findViewById(R.id.heightEditText) // EditText로 타입을 주면서 Id를 찾았을 때도 EditText라고 추정
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)  // 명시적으로 타입 설
+        val resultButton = findViewById<Button>(R.id.resultBotton)
+
     }
 }
